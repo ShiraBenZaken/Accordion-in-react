a0d9d36fe0ea89a08488d3585f26ff7e19b91f8a
ShiraBenZaken <shira441192@gmail.com>
2021-09-20 12:48:41 +0300

    Revert "Update Search.js"
    This reverts commit ffa35ad13900b500dbe03fe8f655e767e63ff30f.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,57 +4,31 @@ import axios from 'axios';
 const Search = () => {
 
     const [term, setTerm] = useState('programming');
-    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setDebouncedTerm(term)
-        }, 500);
 
-        return () => {
-            clearTimeout(timeoutId);
-        }
-    }, [term]);
-
+    console.log(results);
+    
     useEffect(() => {
         /*
         there is short option to write function to call axios (make request) in useEfect() 
         instead [ const search = async ()=> ] and calling after with [ search(); ] 
         write (async () => { await axios.get('api') })()
         */
-        const search = async () => {
+        (async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: debouncedTerm,
+                    srsearch: term,
                 },
             });
             setResults(data.query.search);
-        };
-        if (debouncedTerm) {
-            search();
-        }
-    }, [debouncedTerm]);
+        })();
+    }, [term]);
 
-    const renderedResults = results.map((result) => {
-        return (
-            <div className="item" key={result.pageid}>
-                <div className="right floated content">
-                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
-                </div>
-                <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
-                </div>
-            </div>
-        )
-    });
 
     return (
         <div>
@@ -66,9 +40,6 @@ const Search = () => {
                         value={term}
                         onChange={(e) => setTerm(e.target.value)}
                     />
-                </div>
-                <div className="ui celled list">
-                    {renderedResults}
                 </div>
             </div>
         </div>
