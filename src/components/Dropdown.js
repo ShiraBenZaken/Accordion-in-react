723f12fdf43368062875620871150aefff266b64
ShiraBenZaken <shira441192@gmail.com>
2021-10-03 19:21:10 +0300

    Update Dropdown.js

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener("click", (event) => {
+            setOpen(false);
+        },
+            {
+                capture: true
+            }
+        );
+    }, []);
 
     const renderedOptions = options.map((option) => {
-        if(option.value == selected.value)
+        if (option.value == selected.value)
             return null;
 
         return (
