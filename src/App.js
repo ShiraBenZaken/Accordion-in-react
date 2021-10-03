77dbf932e9ba20c2cbcdd983e244fe66cc558abb
ShiraBenZaken <shira441192@gmail.com>
2021-10-03 18:58:51 +0300

    dropdown
    lesson 180

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
+
 
 const items = [{
     title: 'What is React',
     content: 'React is a FrontEnd javascript framework'
-},{
+}, {
     title: 'Why use React',
     content: 'React is very a simple and lightweight library that only deals with Fast Learning Curve.'
-},{
+}, {
     title: 'How do you use React',
     content: 'You use React by creating components'
-}]
+}];
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    }
+];
 
 
-const App =()=> {
+const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            <br/>
+            <br />
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+            />
         </div>
     );
 }
